8e8dcd2ee8acb5defbaed98651ace310d38231fe
mickymoucs <mkycskwcst@gmail.com>
2023-02-08 12:19:19 +0700

    initial frontend

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react'
+// import { getRoom1 } from '../services/rooms'
+// import { getRoom2 } from '../services/rooms'
+// import { getRoom3 } from '../services/rooms'
+
+const Home=()=> {
+    /*
+    useEffect(() => {
+        getRoom1()
+        getRoom2()
+        getRoom3()
+    }, [])
+    */
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
+export default Home
---
+++ src/services/rooms.jsx
@@ -0,0 +1,32 @@
+/*
+import axios from "axios"
+
+export async function getRoom1() {
+    const res = await axios.get("")
+    return res.data
+}
+
+export async function getRoom2() {
+    const res = await axios.get("")
+    return res.data
+}
+
+export async function getRoom3() {
+    const res = await axios.get("")
+    return res.data
+}
+export async function postRoom1() {
+    const res = await axios.post("")
+    return res.data
+}
+
+export async function postRoom2() {
+    const res = await axios.post("")
+    return res.data
+}
+
+export async function postRoom3() {
+    const res = await axios.post("")
+    return res.data
+}
+*/
