17682ed1453e2ad0f0534f24c9aa8a2b42e07490
thanidacwn <92778351+thanidacwn@users.noreply.github.com>
2023-02-08 14:50:53 +0700

    get and post from hardware

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from routers import rooms
 
-
 app = FastAPI()
 app.include_router(rooms.router)
 
@@ -10,4 +9,3 @@ app.include_router(rooms.router)
 def root():
     """ Root page of the API """
     return {"msg": "welcome to group8 smart home root page สุดเท่"}
-
