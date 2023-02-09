a1ebc4857502c291aac9e9e081c5d9b47ddc671e
thanidacwn <92778351+thanidacwn@users.noreply.github.com>
2023-02-09 13:04:29 +0700

    add cors

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from routers import rooms
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(rooms.router)
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
